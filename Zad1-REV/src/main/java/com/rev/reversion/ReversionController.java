f956f2ace3f723e4e445059b59efd0267f9f30f8
3elk4 <izaa1109@gmail.com>
2020-10-20 14:49:22 +0200

    refactor ReversionController by making StringBuilder field private

--- Zad1-REV/src/main/java/com/rev/reversion/ReversionController.java
+++ Zad1-REV/src/main/java/com/rev/reversion/ReversionController.java
@@ -6,9 +6,11 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ReversionController {
+    private StringBuilder sb = null;
+
     @GetMapping("/reversed/{text}")
     public String getReversed(@PathVariable("text") String text){
-        StringBuilder sb = new StringBuilder(text);
+        sb = new StringBuilder(text);
         sb.reverse();
         return sb.toString();
     }
