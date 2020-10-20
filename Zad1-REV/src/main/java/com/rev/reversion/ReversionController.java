53742424af0fdacfc20a6900af38150db61957f0
3elk4 <izaa1109@gmail.com>
2020-10-20 14:40:44 +0200

    add empty function for reversing string by parameter given in endpoint

--- Zad1-REV/src/main/java/com/rev/reversion/ReversionController.java
+++ Zad1-REV/src/main/java/com/rev/reversion/ReversionController.java
@@ -1,7 +1,13 @@
 package com.rev.reversion;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ReversionController {
+    @GetMapping("/reversed/{text}")
+    public String getReversed(@PathVariable("text") String text){
+        return "";
+    }
 }
