c3af4a83c42280e265372bb25ee9ef750dc92075
3elk4 <izaa1109@gmail.com>
2020-11-03 14:24:02 +0100

    prepare base for string api project

---
+++ Zad2-StringAPI/src/test/java/com/springapi/charlogger/CharloggerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springapi.charlogger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CharloggerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
