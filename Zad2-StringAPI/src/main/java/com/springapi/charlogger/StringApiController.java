98a0e36b3417b31130aeff64b18a0fbf6845c43f
3elk4 <izaa1109@gmail.com>
2020-11-03 14:45:22 +0100

    fill function for getting checked string with code checking sign by sign

--- Zad2-StringAPI/src/main/java/com/springapi/charlogger/StringApiController.java
+++ Zad2-StringAPI/src/main/java/com/springapi/charlogger/StringApiController.java
@@ -4,6 +4,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
 import java.util.Map;
 
 @RestController
@@ -11,6 +12,13 @@ public class StringApiController {
 
     @GetMapping("/{text}")
     public Map<Character, Integer> getStringLog(@PathVariable("text") String text){
-        
+        Map<Character, Integer> signs = new HashMap<Character, Integer>();
+        for (char sign : text.toCharArray()){
+            if(Character.isUpperCase(sign)) signs.merge('B', 1, Integer::sum);
+            else if(Character.isLowerCase(sign)) signs.merge('L', 1, Integer::sum);
+            else if(Character.isDigit(sign)) signs.merge('N', 1, Integer::sum);
+            else signs.merge('S', 1, Integer::sum);
+        }
+        return signs;
     }
 }
