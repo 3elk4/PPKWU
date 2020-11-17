68a23f28bd01bad758baa1703e5d10f1b75882d0
3elk4 <izaa1109@gmail.com>
2020-11-17 15:03:14 +0100

    prepare controller class for connecting with client by browser

---
+++ Zad3-MobCal/src/main/java/com/mobcal/mobcal/CalendarController.java
@@ -0,0 +1,13 @@
+package com.mobcal.mobcal;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CalendarController {
+    @GetMapping("/{year}/{month}")
+    public String getMonthCalendar(@PathVariable("year") Integer year, @PathVariable("month") Integer month){
+        return "";
+    }
+}
