33a703afcb8b008ae5683c5633c998eb004748ba
3elk4 <izaa1109@gmail.com>
2020-11-17 15:15:50 +0100

    pull information about weeia calendar from weeia website

--- Zad3-MobCal/src/main/java/com/mobcal/mobcal/WEEIAMonthCalendar.java
+++ Zad3-MobCal/src/main/java/com/mobcal/mobcal/WEEIAMonthCalendar.java
@@ -1,10 +1,24 @@
 package com.mobcal.mobcal;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+
+import java.io.IOException;
+
 public class WEEIAMonthCalendar {
     private String url = "http://www.weeia.p.lodz.pl";
+    private String calendarId = "kalendarz";
+
     private int year, month;
 
     public WEEIAMonthCalendar(int year, int month){
+        this.year = year;
+        this.month = month;
+    }
 
+    public void GenerateMonthData() throws IOException {
+        Document doc = Jsoup.connect(url).get();
+        Element calendar = doc.getElementById(calendarId);
     }
 }
