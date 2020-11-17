ead101c85f138de867d98447c812e33c5b748d5c
3elk4 <izaa1109@gmail.com>
2020-11-17 14:24:31 +0100

    prepare base for project

---
+++ Zad3-MobCal/src/main/java/com/mobcal/mobcal/MobcalApplication.java
@@ -0,0 +1,13 @@
+package com.mobcal.mobcal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MobcalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MobcalApplication.class, args);
+    }
+
+}
---
+++ Zad3-MobCal/src/test/java/com/mobcal/mobcal/MobcalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mobcal.mobcal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MobcalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
