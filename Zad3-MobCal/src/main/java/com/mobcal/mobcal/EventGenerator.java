bfef636f673ac68f2b54b8273163b7ccfb1e55e1
3elk4 <izaa1109@gmail.com>
2020-12-01 14:47:42 +0100

    create function to generate datetime start of event

--- Zad3-MobCal/src/main/java/com/mobcal/mobcal/EventGenerator.java
+++ Zad3-MobCal/src/main/java/com/mobcal/mobcal/EventGenerator.java
@@ -1,5 +1,6 @@
 package com.mobcal.mobcal;
 
+import net.fortuna.ical4j.model.Date;
 import net.fortuna.ical4j.model.component.CalendarComponent;
 import net.fortuna.ical4j.model.component.VEvent;
 import org.jsoup.nodes.Element;
@@ -7,7 +8,13 @@ import org.jsoup.nodes.Element;
 public class EventGenerator {
 
     public static CalendarComponent Generate(Element day, int month, int year){
-        VEvent event = new VEvent();
+        String name = day.getElementsByClass("InnerBox").text();
+        int dayNumber = Integer.parseInt(day.getElementsByTag("a").text());
+        VEvent event = new VEvent(CreateEventStart(dayNumber, month, year), name);
         return event;
+    }
+
+    private static Date CreateEventStart(int day, int month, int year){
+        return new Date();
     }
 }
