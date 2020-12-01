8b4a28b845cf2a96d2f1c28fae7f36ff3f843667
3elk4 <izaa1109@gmail.com>
2020-12-01 14:59:56 +0100

    add uid generator to EventGenerator class
    add uid to every event component

--- Zad3-MobCal/src/main/java/com/mobcal/mobcal/EventGenerator.java
+++ Zad3-MobCal/src/main/java/com/mobcal/mobcal/EventGenerator.java
@@ -4,6 +4,9 @@ import net.fortuna.ical4j.model.Date;
 import net.fortuna.ical4j.model.TimeZone;
 import net.fortuna.ical4j.model.component.CalendarComponent;
 import net.fortuna.ical4j.model.component.VEvent;
+import net.fortuna.ical4j.model.property.Uid;
+import net.fortuna.ical4j.util.RandomUidGenerator;
+import net.fortuna.ical4j.util.UidGenerator;
 import org.jsoup.nodes.Element;
 
 import java.util.Calendar;
@@ -12,9 +15,11 @@ public class EventGenerator {
     private static final Calendar cal = java.util.Calendar.getInstance();
     private static final String TitleClassName = "InnerBox";
     private static final String DayTagName = "a";
+    private static UidGenerator ug = new RandomUidGenerator();
 
     public static CalendarComponent Generate(Element day, int month, int year){
         VEvent event = new VEvent(CreateEventStart(Day(day), month, year), Name(day));
+        event.getProperties().add(ug.generateUid());
         return event;
     }
 
