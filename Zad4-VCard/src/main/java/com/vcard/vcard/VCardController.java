a059598b725ee10ba12135d44771e335fcf78f77
3elk4 <izaa1109@gmail.com>
2020-12-15 15:30:05 +0100

    prepare basics for endpoint to generate vcard and write it down to file

--- Zad4-VCard/src/main/java/com/vcard/vcard/VCardController.java
+++ Zad4-VCard/src/main/java/com/vcard/vcard/VCardController.java
@@ -4,13 +4,26 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 public class VCardController {
+    List<VCardModel> vcards = null;
+
     @GetMapping("/{trade}")
-    public String getTrades(@PathVariable("trade") String trade) {
+    public List<VCardModel> getTrades(@PathVariable("trade") String trade) {
        ServiceOffersGenerator sog = new ServiceOffersGenerator(trade);
-       sog.GenerateVCards();
-       return "";
+       var vcards = sog.GenerateVCards();
+       return vcards;
+    }
+
+    @GetMapping("/offers/{id}")
+    public String generateVcf(@PathVariable("id") int id){
+        if(vcards == null || vcards.isEmpty()) return "Oops! There was a problem to create VCF file.";
+
+        var vcard = vcards.get(id);
+        //generate vcard + zapisz plik
+        return "File was created successfully!";
     }
 
 }
