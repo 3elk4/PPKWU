c16a9becff06f62922655df0b8c7d9e5f7167b59
3elk4 <izaa1109@gmail.com>
2020-12-15 15:54:47 +0100

    refactor method generating vcard

--- Zad4-VCard/src/main/java/com/vcard/vcard/VCardGenerator.java
+++ Zad4-VCard/src/main/java/com/vcard/vcard/VCardGenerator.java
@@ -2,9 +2,7 @@ package com.vcard.vcard;
 
 import ezvcard.Ezvcard;
 import ezvcard.VCard;
-import ezvcard.property.Address;
-import ezvcard.property.FormattedName;
-import ezvcard.property.Organization;
+import ezvcard.property.*;
 
 import java.io.File;
 import java.io.IOException;
@@ -12,12 +10,11 @@ import java.io.IOException;
 public class VCardGenerator {
     public static VCard GenerateVCard(VCardModel model){
         VCard vcard = new VCard();
-        var org= new Organization();
-        org.setSortAs(model.CompanyName);
-        vcard.addOrganization(org);
-        vcard.addEmail();
-        vcard.addTelephoneNumber();
-        vcard.addAddress();
+
+        vcard.addOrganization(PrepareCompanyName(model.CompanyName));
+        vcard.addEmail(PrepareEmail(model.Email));
+        vcard.addTelephoneNumber(PreparePhoneNumber(model.PhoneNumber));
+        vcard.addAddress(PrepareAddress(model.Address));
         return vcard;
     }
 
@@ -34,5 +31,25 @@ public class VCardGenerator {
 
     private static String CreateFileName(String name){
         return name + ".vcf";
+    }
+
+    private static Address PrepareAddress(String address){
+        var result = new Address();
+        result.setStreetAddress(address);
+        return result;
+    }
+
+    private static Telephone PreparePhoneNumber(String phoneNumber){
+        return new Telephone(phoneNumber);
+    }
+
+    private static Email PrepareEmail(String email){
+        return new Email(email);
+    }
+
+    private static Organization PrepareCompanyName(String companyName){
+        var org = new Organization();
+        org.setSortAs(companyName);
+        return org;
     }
 }
