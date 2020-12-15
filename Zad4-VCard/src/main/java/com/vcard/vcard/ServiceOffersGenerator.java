d47957838eb5d80593e465a7af9afe740109798e
3elk4 <izaa1109@gmail.com>
2020-12-15 14:52:41 +0100

    fill function for getting offers from website
    check given offers

--- Zad4-VCard/src/main/java/com/vcard/vcard/ServiceOffersGenerator.java
+++ Zad4-VCard/src/main/java/com/vcard/vcard/ServiceOffersGenerator.java
@@ -1,6 +1,18 @@
 package com.vcard.vcard;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class ServiceOffersGenerator {
+    private String companyListId = "company-list";
+    private String companyNameId = "company-name";
+    private String offerId = "li";
+
     private String URL(){
         return "https://panoramafirm.pl/szukaj?k=" + trade;
     }
@@ -8,5 +20,23 @@ public class ServiceOffersGenerator {
     private String trade;
     public ServiceOffersGenerator(String trade){
         this.trade = trade;
+    }
+
+    public List<VCardModel> GenerateVCards(){
+        List<VCardModel> vcardsData = new ArrayList<>();
+
+        try {
+            Document doc = Jsoup.connect(URL()).get();
+            Element offers = doc.getElementById(companyListId);
+            for (Element offer : offers.getElementsByTag(offerId)) {
+                System.out.println(offer.getElementsByClass(companyNameId).text());
+//                VCardModel vcard = new VCardModel();
+//                vcardsData.add(vcard);
+            }
+
+        }catch(IOException e){
+            System.out.println(e.getMessage());
+        }
+        return vcardsData;
     }
 }
