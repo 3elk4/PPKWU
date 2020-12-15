354ab115343a759f5496e8c97a9910b96153fc73
3elk4 <izaa1109@gmail.com>
2020-12-15 15:42:18 +0100

    add function to write file down with generated vcard

--- Zad4-VCard/src/main/java/com/vcard/vcard/VCardGenerator.java
+++ Zad4-VCard/src/main/java/com/vcard/vcard/VCardGenerator.java
@@ -1,15 +1,34 @@
 package com.vcard.vcard;
 
+import ezvcard.Ezvcard;
 import ezvcard.VCard;
 import ezvcard.property.Address;
 
+import java.io.File;
+import java.io.IOException;
+
 public class VCardGenerator {
-    public static VCard GenerateVcard(VCardModel model){
+    public static VCard GenerateVCard(VCardModel model){
         VCard vcard = new VCard();
         vcard.addFormattedName();
         vcard.addEmail();
         vcard.addTelephoneNumber();
         vcard.addAddress();
         return vcard;
+    }
+
+    public static boolean GenerateVCF(VCard vcard, String name){
+        String filename = CreateFileName(name);
+        File file = new File(filename);
+        try {
+            Ezvcard.write(vcard).go(file);
+        } catch (IOException e) {
+            return false;
+        }
+        return true;
+    }
+
+    private static String CreateFileName(String name){
+        return name + ".vcf";
     }
 }
